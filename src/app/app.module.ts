bc1b15d3626429d629c7a93c71bc2d97c0feb652
DucChien-Monkey <chien.nguyen@hoc10.com>
2022-09-08 00:25:57 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ModalsComponent } from './modals/modals.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      ModalsComponent,      
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+     
   ],
   providers: [],
   bootstrap: [AppComponent]
